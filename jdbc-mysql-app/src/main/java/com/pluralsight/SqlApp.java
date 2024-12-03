321e3f2e47e210ff4567d7a4e03ddd2143a1650e
Damian <jerezdamian640@gmail.com>
2024-12-03 14:23:37 -0500

    updated for using basic database

--- jdbc-mysql-app/src/main/java/com/pluralsight/SqlApp.java
+++ jdbc-mysql-app/src/main/java/com/pluralsight/SqlApp.java
@@ -1,31 +1,51 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class SqlApp {
 
     public static void main(String[] args) {
 
+        BasicDataSource dataSource;
+        dataSource = new BasicDataSource();
+
         String url = "jdbc:mysql://localhost:3306/cardealership";
-        String user = "root" ;
-        String password = "yearup";
+        dataSource.setUrl(url);
+        dataSource.setUsername(args[0]);
+        dataSource.setPassword(args[1]);
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = dataSource.getConnection()) {
 
-            Statement statement = connection.createStatement();
-            statement.executeQuery("select * from vehicles;");
+            PreparedStatement statement = connection.prepareStatement("select * from vehicles where make = ?;");
 
-            ResultSet resultSet = statement.getResultSet();
-            while (resultSet.next()){
-                String make = resultSet.getString("make");
-                String model = resultSet.getString("model");
-                double price = resultSet.getDouble("price");
-                System.out.printf("make: %s model: %s Price: $%.2f\n", make, model, price);
+            boolean running = true;
+            while (running) {
+                System.out.print("Enter a vehicle by make: ");
+                String command = scanner.nextLine();
+
+                statement.setString(1, command);
+                statement.execute();
+
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    String make = resultSet.getString("make");
+                    String model = resultSet.getString("model");
+                    double price = resultSet.getDouble("price");
+                    System.out.printf("make: %s model: %s Price: $%.2f\n", make, model, price);
+                }
             }
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            dataSource.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
